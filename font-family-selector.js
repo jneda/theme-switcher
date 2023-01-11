816919c882e7e88b9094bbb9c57b6fad3a652c50
jdane <julien.dane@gmail.com>
2023-01-11 14:23:38 +0100

    feat: select quote font family

---
+++ font-family-selector.js
@@ -0,0 +1,40 @@
+const fontFamilies = [
+  'Dancing Script',
+  'Nixie One',
+  'Courgette',
+  'Merriweather'
+];
+
+function selectFontFamily() {
+  toggleFontFamilySelector();
+  fontFamilies.forEach(fontFamily => showCaseFont(fontFamily));
+}
+
+function toggleFontFamilySelector() {
+  fontFamilyDrawer.innerHTML = null;
+  fontFamilyDrawer.classList.toggle('hidden');
+}
+
+function showCaseFont(fontFamily) {
+  const pElement = document.createElement('p');
+  pElement.textContent = 'Bâchez quelques wagons-taxis avec vos pyjamas de fakir';
+  pElement.style.fontFamily = fontFamily;
+  pElement.setAttribute('name', fontFamily);
+  fontFamilyDrawer.appendChild(pElement);
+
+  pElement.addEventListener('click', updateQuoteFont);
+}
+
+function updateQuoteFont(event) {
+  console.log(event);
+  const fontFamily = event.target.getAttribute('name');
+  const quoteTextElements = document.querySelectorAll('.quote p, .quote a');
+  console.log(quoteTextElements);
+  quoteTextElements.forEach(element => element.style.fontFamily = fontFamily);
+  toggleFontFamilySelector();
+}
+
+const fontFamilyDrawer = document.querySelector('.font-family-drawer');
+
+const fontFamilySelectorButton = document.querySelector('#font-family-button');
+fontFamilySelectorButton.addEventListener('click', selectFontFamily);
